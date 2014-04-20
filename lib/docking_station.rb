26e591d784846a8b5a141fa1d5ba6837c678f07f
Emma <emma_williams1@hotmail.co.uk>
2014-04-20 14:19:52 +0100

    docing station can release bike

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -8,4 +8,9 @@ class DockingStation
     #do you have any element-if it says true then you do have bikes becuase there are bikes in it.
   end
 
+  def release_bike
+    @bikes.pop unless @bikes.last.broken?
+    #popping an element out of there
+  end
+
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,14 +1,32 @@
 require 'docking_station'
 
 describe DockingStation do
+  let (:bike) { double :bike, broken?: false }
+  let(:station) { DockingStation.new([bike]) }
+  
 
   it 'has no bikes' do 
     expect(DockingStation.new).not_to have_bikes
   end
 
   it 'can be created with bikes' do 
-    station = DockingStation.new([:bike, :bike])
     expect(station).to have_bikes
     end
+
+    it 'can release a bike' do
+      station.release_bike
+      expect(station).not_to have_bikes
+  end
+  #if you release a bike should not have bikes
   
+  it 'cannot release a bike that is broken' do
+    broken_bike = double :bike, broken?: true
+    #the double stands in so we can test this
+    station = DockingStation.new([broken_bike])
+
+    station.release_bike
+
+    expect(station).to have_bikes
+  end
+
 end
