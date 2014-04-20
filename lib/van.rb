d9715f3486fd054edf2399de9fbf10dbbd07230d
Emma <emma_williams1@hotmail.co.uk>
2014-04-20 22:27:31 +0100

    more work on van spec and lib

--- lib/van.rb
+++ lib/van.rb
@@ -1,4 +1,25 @@
 class Van
-def has_bikes?
+  attr_reader :bikes
+
+  def initialize(bikes=[])
+    #if the array is empty it has no bikes-nil false
+    @bikes = bikes
   end
+
+def has_bikes?
+  @bikes.any?
+  #expected has bikes to return false got true-fixed
+  end
+
+  def pickup_broken_bikes_from(place)
+    @bikes.concat(place.release_broken_bikes)
+  end
+
+  def drop_broken_bikes_into(place)
+    @bikes.select {|bike| bike.broken?}.each{|bike| place.dock(@bikes.delete(bike)) }
+    
+  end
+
+
+
 end
--- spec/van_spec.rb
+++ spec/van_spec.rb
@@ -1,8 +1,72 @@
 require 'van'
 
 describe Van do
+  let(:van) { Van.new }
+
   it 'has no bikes' do 
     expect(Van.new).not_to have_bikes
   end
+
+  it 'picks up broken bikes from a place' do 
+    place = double :place
+
+    expect(place).to receive(:release_broken_bikes).and_return([])
+    
+    van.pickup_broken_bikes_from(place)
+  end
+
+  it 'has broken bikes after picking them up' do 
+    place = double :place, release_broken_bikes: [:bike]
+
+    van.pickup_broken_bikes_from(place)
+
+    expect(van).to have_bikes
+  end
+
+  it 'shows us the bikes it has'  do 
+    expect(Van.new([:bike]).bikes).to eq [:bike]
+  end
+
+  it 'has some more bikes after picking some up' do
+   place = double :place, release_broken_bikes: [:bike]
+  place2 = double :place, release_broken_bikes: [:bike]
   
+  van.pickup_broken_bikes_from(place)
+  van.pickup_broken_bikes_from(place2)
+
+  expect(van.bikes).to eq [:bike, :bike]
 end
+
+it 'drops broken bikes to a place' do
+place = double :place
+van = Van.new([:bike])
+
+expect(place).to receive(:dock)
+
+van.drop_broken_bikes_into(place)
+end
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
