1c17b1bfc873d793b8d93a454033c4421ad8798e
Emma <emma_williams1@hotmail.co.uk>
2014-04-20 13:08:14 +0100

    added methods

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,3 +1,11 @@
 class DockingStation
+  def initialize(bikes = [])
+    @bikes = bikes 
+  end
+
+  def has_bikes?
+    @bikes.any?
+    #do you have any element-if it says true then you do have bikes becuase there are bikes in it.
+  end
 
 end
