432feaeae153d6ff136c2dbe181bdeb0fd7be595
Emma <emma_williams1@hotmail.co.uk>
2014-04-20 19:13:34 +0100

    refactored and adding of broken bikes to the docking station

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -13,6 +13,11 @@ class DockingStation
     #all the ones that are not broken are available bikes/rejecting broken bikes
   end
 
+  def broken_bikes
+        @bikes.select {|bike| bike.broken?}
+  end
+
+
   def release_bike
     @bikes.delete(available_bikes.pop)
     #popping an element out of there
@@ -22,6 +27,10 @@ class DockingStation
   def dock(bike)
     @bikes << bike
     nil
+  end
+
+  def release_broken_bikes
+broken_bikes.map{|bike| @bikes.delete(bike) }
   end
 
 end
